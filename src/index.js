b7ae4fdaf106d64c526ed799c8310c25d1dd95eb
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-18 21:23:25 +0300

    new server configuration

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: false }));
 app.use(router);
 
 initializeDatabase()
